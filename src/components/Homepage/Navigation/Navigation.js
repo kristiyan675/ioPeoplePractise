0501a64f8f2248f1e1a646b662e30c8f80828511
Milena Valkova <milena.valkova@weareyou.com>
2022-07-04 17:02:24 +0300

    style changes

--- src/components/Homepage/Navigation/Navigation.js
+++ src/components/Homepage/Navigation/Navigation.js
@@ -9,42 +9,44 @@ import {
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../../../images/logo.svg";
 import "./Navigation.scss";
-import classes from "./Navigation.module.scss";
+
+// import classes from "./Navigation.module.scss";
 const Navigation = (props) => {
   return (
-    <Navbar expand="lg" bg="primary" variant="dark">
-      <Container>
+    <Navbar expand="lg" variant="dark" >
+      <Container fluid>
         <Link to="/">
           <Navbar.Brand as="img" src={Logo} />
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-            <NavLink to="/" className="nav-link">
+            <NavLink to="/" className="nav-link display-6">
               Home
             </NavLink>
-
-            <NavLink to="/" className="nav-link">
+            <NavLink to="/" className="nav-link display-6">
               Profile
             </NavLink>
-            <NavLink to="/" className="nav-link">
+            <NavLink to="/" className="nav-link display-6">
               My vacations
             </NavLink>
-            <NavLink to="/" className="nav-link">
+            <NavLink to="/" className="nav-link display-6">
               Docs
             </NavLink>
-            <NavLink to="/" className="nav-link">
+            <NavLink to="/" className="nav-link display-6">
               Contacts
             </NavLink>
             <NavDropdown
-              title={<Image src={Logo} width="20%" roundedCircle={true} />}
+              title={<Image src={Logo} width="30%" roundedCircle={true} />}
               id="basic-nav-dropdown"
             >
               <NavDropdown.Item eventKey="1">Log Out</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+        
       </Container>
+      
     </Navbar>
   );
 };
